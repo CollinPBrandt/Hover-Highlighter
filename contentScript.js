bb83450c31d27810662724232622a806e3317e74
Collin <Collin.brandt1@gmail.com>
2018-08-20 14:33:10 -0500

    Refactored contentScript to only blast a paragraph when it is hovered over. Also removes blast spans when toggle is turned off.

--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,56 @@
 var hoverStyle = "onhover";
+var onToggle;
 //listen for messages
 chrome.runtime.onMessage.addListener(function(request) {
-    let onToggle = JSON.parse(request.onToggle);
+    onToggle = JSON.parse(request.onToggle);
     //if message contains onToggle, check status, turn highlighter on/off
     if(onToggle){
-        $("p").blast({delimiter: "word"});
-        addHoverClass();
-    }
-    else{
-        $("p").blast(false);
-        removeHoverClass();
+        blastParagraphs();
+        }
+    else {
+        unBlastParagraphs();
     }
 });
 
-function addHoverClass(){
-    $(".blast").on("mouseenter", function(){$(this).addClass(hoverStyle);})
-    $(".blast").on("mouseleave", function(){$(this).removeClass(hoverStyle);})
+//when hovering over a p tag
+/*$("p").mouseenter(function(){
+    //if the onToggle is true and the p tag has not already been blasted, add blast tag and hover classes
+   if(onToggle && !$(this).hasClass("blast-root")){
+       $(this).blast({delimiter: "word"});
+       addHoverClass();
+   }
+});
+
+$("p").mouseleave(function(){
+    //if the onToggle is true and the p tag has not already been blasted, add blast tag and hover classes
+    if(!onToggle && $(this).hasClass("blast-root")){
+        $(this).blast({delimiter: "word"});
+        addHoverClass();
+    }
+});*/
+
+function blastParagraphs(){
+    $("p").on("mouseenter", blastHoveredParagraph);
 }
 
-function removeHoverClass(){
-    $(".blast").off("mouseenter");
-    $(".blast").off("mouseleave");
+function unBlastParagraphs(){
+    $("p").off("mouseenter", blastHoveredParagraph);
+    $("p").blast(false);
+}
+
+function blastHoveredParagraph(){
+    if(!$(this).hasClass("blast-root")){
+        $(this).blast({delimiter: "word"});
+        addHoverClass();
+    }
+}
+
+function addHoverClass(){
+    $(".blast").mouseenter(function(){
+        $(this).addClass(hoverStyle);
+    });
+
+    $(".blast").mouseleave(function(){
+        $(this).removeClass(hoverStyle);
+    });
 }
